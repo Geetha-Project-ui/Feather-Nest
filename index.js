ebf24e4a95342758d0bbc71028328c6835e36ec5
Geetha-Project-ui <geetharamaraj2095@gmail.com>
2024-07-11 18:20:41 +0530

    Added all files

---
+++ index.js
@@ -0,0 +1,12 @@
+
+document.getElementById('show-sign-up').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('sign-in-form').classList.add('d-none');
+    document.getElementById('sign-up-form').classList.remove('d-none');
+});
+
+document.getElementById('show-sign-in').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('sign-up-form').classList.add('d-none');
+    document.getElementById('sign-in-form').classList.remove('d-none');
+});
