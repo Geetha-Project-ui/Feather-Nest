0138a143b69a8593e6fbf1cb3fa62df26dcd1fd1
Geetha-Project-ui <geetharamaraj2095@gmail.com>
2024-08-03 22:39:29 +0530

    Side bar fixed and added other pages

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ document.getElementById('show-sign-in').addEventListener('click', function(event
 });
 
 
+$("#leftside-navigation .sub-menu > a").click(function(e) {
+    $("#leftside-navigation ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
+    e.stopPropagation()
+  })
